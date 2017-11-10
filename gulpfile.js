7f58dcb8e41bf752b81d906635dc72956450ad34
Vlad <warvlad822@gmail.com>
2017-11-10 21:27:30 +0200

    git push

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
-'use strict'
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const pug = require('gulp-pug');
 const sass = require('gulp-sass');
 const spritesmith = require('gulp.spritesmith');
-// const myCustomFS = require('some-custom-fs');
+const rimraf = require('rimraf');
+const rename = require('gulp-rename');
 
-// Static server
+/* -------- Server  -------- */
 gulp.task('server', function () {
     browserSync.init({
         server: {
@@ -15,89 +15,69 @@ gulp.task('server', function () {
         }
     });
 
-
     gulp.watch('build/**/*').on('change', browserSync.reload);
 });
 
-/**
- * Pug compile
- */
+/* ------------ Pug compile ------------- */
 gulp.task('templates:compile', function buildHTML() {
     return gulp.src('source/template/index.pug')
         .pipe(pug({
             pretty: true
-            // Your options in here. 
         }))
-        .pipe(gulp.dest('build'));
+        .pipe(gulp.dest('build'))
 });
 
-/**
- * Sass compile
- */
-
-gulp.task('style:compile', function () {
+/* ------------ Styles compile ------------- */
+gulp.task('styles:compile', function () {
     return gulp.src('source/styles/main.scss')
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(rename('main.min.css'))
         .pipe(gulp.dest('build/css'));
 });
 
-
-/**
- * Sprite
- */
-gulp.task('sprite', function () {
-    var spriteData = gulp.src('source/img/icons/*.png').pipe(spritesmith({
+/* ------------ Sprite ------------- */
+gulp.task('sprite', function (cb) {
+    const spriteData = gulp.src('source/images/icons/*.png').pipe(spritesmith({
         imgName: 'sprite.png',
         imgPath: '../images/sprite.png',
         cssName: 'sprite.scss'
     }));
 
-    spriteData.img.pipe(gulp.dest('build/img'));
-    spriteData.img.pipe(gulp.dest('source/styles/global/'));
-    cd();
+    spriteData.img.pipe(gulp.dest('build/images/'));
+    spriteData.css.pipe(gulp.dest('source/styles/global/'));
+    cb();
 });
 
+/* ------------ Delete ------------- */
+gulp.task('clean', function del(cb) {
+    return rimraf('build', cb);
+});
 
-/**
- * Delete
- */
-
-// gulp.task('clean', function del(cd) {
-//     return rimraf('build', myCustomFS, cd);
-// });
-
-/**
- * Copy fonts
- */
-
+/* ------------ Copy fonts ------------- */
 gulp.task('copy:fonts', function () {
     return gulp.src('./source/fonts/**/*.*')
         .pipe(gulp.dest('build/fonts'));
 });
 
-
-/**
- * Copy images
- */
-
-gulp.task('copy:imgase', function () {
-    return gulp.src('./source/img/**/*.*')
-        .pipe(gulp.dest('build/img'));
+/* ------------ Copy images ------------- */
+gulp.task('copy:images', function () {
+    return gulp.src('./source/images/**/*.*')
+        .pipe(gulp.dest('build/images'));
 });
 
-gulp.task('copy', gulp.parallel('copy:fonts', 'copy:imgase'));
+/* ------------ Copy ------------- */
+gulp.task('copy', gulp.parallel('copy:fonts', 'copy:images'));
 
-
-/**
- * Wathers
- */
-
+/* ------------ Watchers ------------- */
 gulp.task('watch', function () {
     gulp.watch('source/template/**/*.pug', gulp.series('templates:compile'));
-    gulp.watch('source/styles/**/*.scss', gulp.series('style:compile'));
+    gulp.watch('source/styles/**/*.scss', gulp.series('styles:compile'));
 });
 
-gulp.task('default'.gulp.series(
-    gulp.parallel('templates:compile', 'sass:compile', 'sprite', 'copy'),
+gulp.task('default', gulp.series(
+    'clean',
+    gulp.parallel('templates:compile', 'styles:compile', 'sprite', 'copy'),
     gulp.parallel('watch', 'server')
 ));

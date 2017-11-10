c769e6fb4ec8c44a46c6ac5ec02b3a5a5a293959
Vlad <warvlad822@gmail.com>
2017-11-10 20:19:29 +0200

    git push

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,24 @@
 'use strict'
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const pug = require('gulp-pug');
+
+// Static server
+gulp.task('server', function () {
+    browserSync.init({
+        server: {
+            port: 9000,
+            baseDir: "build"
+        }
+    });
+
+
+    gulp.watch('build/**/*').on('change', browserSync.reload);
+});``
+
+gulp.task('views', function buildHTML() {
+    return gulp.src('views/*.pug')
+        .pipe(pug({
+            // Your options in here. 
+        }))
+});
